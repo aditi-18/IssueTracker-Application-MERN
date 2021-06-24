d2d0d937b4f8a39e27c54177ab9460feefeca330
aditishri1896 <shrivastava.adi@northeastern.edu>
2021-06-24 12:36:31 -0400

    Updating UI server and APi server

--- api/server.js
+++ api/server.js
@@ -100,7 +100,7 @@ async function issueAdd(_, { issue }) {
     try {
         await connectToDb();
         app.listen(3000, function(){
-            console.log('App started on port 3000');
+            console.log('API server started on port 3000');
         });
     } catch (err) {
         console.log('ERROR:', err);
@@ -108,7 +108,7 @@ async function issueAdd(_, { issue }) {
 })();
 const server = new ApolloServer({
     
- typeDefs:fs.readFileSync('./schema.graphql', 'utf-8'),
+ typeDefs:fs.readFileSync('schema.graphql', 'utf-8'),
  resolvers,
  formatError: error => {
     console.log(error);
@@ -118,5 +118,4 @@ const server = new ApolloServer({
 
 });
 const app = express();
-app.use(express.static('public'));
 server.applyMiddleware({ app, path: '/graphql' });
