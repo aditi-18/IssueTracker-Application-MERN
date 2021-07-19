9800060ae5ca9d839e87be02146c50ef9cc808f7
aditishri1896 <shrivastava.adi@northeastern.edu>
2021-07-19 13:32:30 -0400

    Sign in UI

---
+++ ui/dist/server.f717341109a3dab6a879.hot-update.js
@@ -0,0 +1,31 @@
+exports.id = "server";
+exports.modules = {
+
+/***/ "./src/Contents.jsx":
+false,
+
+/***/ "./src/IssueAddNavItem.jsx":
+false,
+
+/***/ "./src/Page.jsx":
+/*!**********************!*\
+  !*** ./src/Page.jsx ***!
+  \**********************/
+/*! exports provided: default */
+/***/ (function(module, exports) {
+
+throw new Error("Module build failed (from ./node_modules/babel-loader/lib/index.js):\nSyntaxError: D:\\CS5610-WebDev\\Book\\ui\\src\\Page.jsx: Missing semicolon. (77:7)\n\n  75 |   );\n  76 | }\n> 77 | {\"mode\":\"full\",\"isActive\":false}\n     |        ^\n    at Object._raise (D:\\CS5610-WebDev\\Book\\ui\\node_modules\\@babel\\parser\\lib\\index.js:816:17)\n    at Object.raiseWithData (D:\\CS5610-WebDev\\Book\\ui\\node_modules\\@babel\\parser\\lib\\index.js:809:17)\n    at Object.raise (D:\\CS5610-WebDev\\Book\\ui\\node_modules\\@babel\\parser\\lib\\index.js:770:17)\n    at Object.semicolon (D:\\CS5610-WebDev\\Book\\ui\\node_modules\\@babel\\parser\\lib\\index.js:9863:10)\n    at Object.parseExpressionStatement (D:\\CS5610-WebDev\\Book\\ui\\node_modules\\@babel\\parser\\lib\\index.js:13081:10)\n    at Object.parseStatementContent (D:\\CS5610-WebDev\\Book\\ui\\node_modules\\@babel\\parser\\lib\\index.js:12670:19)\n    at Object.parseStatement (D:\\CS5610-WebDev\\Book\\ui\\node_modules\\@babel\\parser\\lib\\index.js:12534:17)\n    at Object.parseBlockOrModuleBlockBody (D:\\CS5610-WebDev\\Book\\ui\\node_modules\\@babel\\parser\\lib\\index.js:13123:25)\n    at Object.parseBlockBody (D:\\CS5610-WebDev\\Book\\ui\\node_modules\\@babel\\parser\\lib\\index.js:13114:10)\n    at Object.parseBlock (D:\\CS5610-WebDev\\Book\\ui\\node_modules\\@babel\\parser\\lib\\index.js:13098:10)\n    at Object.parseStatementContent (D:\\CS5610-WebDev\\Book\\ui\\node_modules\\@babel\\parser\\lib\\index.js:12610:21)\n    at Object.parseStatement (D:\\CS5610-WebDev\\Book\\ui\\node_modules\\@babel\\parser\\lib\\index.js:12534:17)\n    at Object.parseBlockOrModuleBlockBody (D:\\CS5610-WebDev\\Book\\ui\\node_modules\\@babel\\parser\\lib\\index.js:13123:25)\n    at Object.parseBlockBody (D:\\CS5610-WebDev\\Book\\ui\\node_modules\\@babel\\parser\\lib\\index.js:13114:10)\n    at Object.parseProgram (D:\\CS5610-WebDev\\Book\\ui\\node_modules\\@babel\\parser\\lib\\index.js:12457:10)\n    at Object.parseTopLevel (D:\\CS5610-WebDev\\Book\\ui\\node_modules\\@babel\\parser\\lib\\index.js:12448:25)\n    at Object.parse (D:\\CS5610-WebDev\\Book\\ui\\node_modules\\@babel\\parser\\lib\\index.js:14175:10)\n    at parse (D:\\CS5610-WebDev\\Book\\ui\\node_modules\\@babel\\parser\\lib\\index.js:14227:38)\n    at parser (D:\\CS5610-WebDev\\Book\\ui\\node_modules\\@babel\\core\\lib\\parser\\index.js:52:34)\n    at parser.next (<anonymous>)\n    at normalizeFile (D:\\CS5610-WebDev\\Book\\ui\\node_modules\\@babel\\core\\lib\\transformation\\normalize-file.js:82:38)\n    at normalizeFile.next (<anonymous>)\n    at run (D:\\CS5610-WebDev\\Book\\ui\\node_modules\\@babel\\core\\lib\\transformation\\index.js:29:50)\n    at run.next (<anonymous>)\n    at Function.transform (D:\\CS5610-WebDev\\Book\\ui\\node_modules\\@babel\\core\\lib\\transform.js:25:41)\n    at transform.next (<anonymous>)\n    at step (D:\\CS5610-WebDev\\Book\\ui\\node_modules\\gensync\\index.js:261:32)\n    at gen.next (D:\\CS5610-WebDev\\Book\\ui\\node_modules\\gensync\\index.js:273:13)\n    at async.call.value (D:\\CS5610-WebDev\\Book\\ui\\node_modules\\gensync\\index.js:223:11)");
+
+/***/ }),
+
+/***/ "./src/Search.jsx":
+false,
+
+/***/ "./src/SignInNavItem.jsx":
+false,
+
+/***/ "react-select/lib/Async":
+false
+
+};
+//# sourceMappingURL=server.f717341109a3dab6a879.hot-update.js.map
--- ui/server/template.js
+++ ui/server/template.js
@@ -7,25 +7,27 @@ export default function template(body, data) {
 <head>
   <meta charset="utf-8">
   <title>Pro MERN Stack</title>
-  <link rel="stylesheet" href="/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="/bootstrap/css/bootstrap.min.css" >
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <script src="https://apis.google.com/js/api:client.js"></script>
 
   <style>
     table.table-hover tr {cursor: pointer;}
     .panel-title a {display: block; width: 100%; cursor: pointer;}
-    background-image: url('new.jpg')
   </style>
 </head>
 
 <body>
-<!-- Page generated from template. -->
-<div id="content" background-image: url('new.jpg')>${body}</div>
+  <!-- Page generated from template. -->
+  <div id="contents">${body}</div>
+  <script>window.__INITIAL_DATA__ = ${serialize(data)}</script>
 
-<script>window.__INITIAL_DATA__ = ${serialize(data)}</script>
-<script src="/env.js"></script>
-<script src="/vendor.bundle.js"></script>
-<script src="/app.bundle.js"></script>
+  <script src="/env.js"></script>
+  <script src="/vendor.bundle.js"></script>
+  <script src="/app.bundle.js"></script>
 </body>
+
 </html>
 `;
 }
